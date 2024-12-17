e4286697b775f0c90dd48319ca1f2a1a6cdc2f03
schnap3380 <123idan1231@gmail.com>
2024-12-17 11:27:40 +0200

    with full queries

--- journal_finder.R
+++ journal_finder.R
@@ -2,8 +2,6 @@ library(tidyverse)
 library(dplyr)
 install.packages('writexl')
 library(writexl)
-install.packages('fedmatch')
-library(fedmatch)
 
 Data <- read.csv("journal_ranking_data.CSV")
 
@@ -40,10 +38,54 @@ cleaned_data$Category <- gsub("\\(miscellaneous\\)", "", cleaned_data$Category)
 cleaned_data$Category <- gsub("\\[", "", cleaned_data$Category)
 cleaned_data$Category <- gsub("\\]", "", cleaned_data$Category)
 cleaned_data$Category <- gsub("\\'", "", cleaned_data$Category)
+cleaned_data$Category <- stringr::str_trim(cleaned_data$Category)
 
 unique(cleaned_data$Category)
 
 unique(cleaned_data[order(cleaned_data$Category),]$Category)
+
+
+#making queries:
+
+
+#I am a business executive who is interested in learning more about what science can teach us about [Insert field]
+#I am a PHD student  who is interested in learning more about what science can teach us about [Insert field]
+
+#what are some things that the field of social cognition has come to a consensus on?
+promts = c('I am a business executive who is interested in learning more about what science can teach us about',
+           'I am a PHD student  who is interested in learning more about what science can teach us about')
+categories <- unique(cleaned_data[order(cleaned_data$Category),]$Category)
+
+
+form <- expand.grid(
+  promt = promts,
+  field = categories
+)
+
+#randomize the rows:
+seed(42)
+form <- form[sample(1:nrow(form),nrow(form), replace=FALSE),]
+
+#making the promts:
+
+queries <- c()
+sen_part_1 <- 'What are some things that the field of'
+sen_part_2 <- 'has come to a consensus on?'
+for (i in 1:nrow(form)) {
+  queries[i] <- paste(form[i,1],form[i,2],". ", sen_part_1, form[i,2],sen_part_2, sep = " ")
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
